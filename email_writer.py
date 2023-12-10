26dd8bfd1ab13aa8348e7f69378555cac710e380
Kwan Suk Kim <linupa@gmail.com>
2023-12-10 08:14:52 -0800

    Added emailSender
    - Also added action test

---
+++ email_writer.py
@@ -0,0 +1,15 @@
+import os
+import jinja2
+
+class EmailWriter():
+  def __init__(self):
+    self.template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "emails")
+
+  def Render(self, template_name, context):
+    return jinja2.Environment(
+      loader=jinja2.FileSystemLoader(self.template_path)
+    ).get_template(template_name).render(context)
+
+  def Write(self, template_name, content):
+    return self.Render(template_name, { 'content' : content })
+
---
+++ sender.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python3
+
+import os
+import time
+import sys
+import threading
+import codecs
+import json
+from gmail3 import GMail
+from datetime import datetime, timedelta
+from mongodb import MongoDB
+
+#os.chdir(sys.path[0])
+#sys.path = ['../gserver'] + sys.path
+from email_writer import EmailWriter
+
+GOOGLE_ACCOUNT = "hkmcc.library@gmail.com"
+
+global subject
+global body
+global msgType
+
+def sendEmail(userList, test):
+  count = 0
+  tokenStr = os.environ["MAIL_TOKEN"]
+  token = json.loads(tokenStr)
+  gmail = GMail(token)
+  logFile = open("emailLog.txt", "a", encoding="utf-8")
+  logFile.write(str(datetime.now()) + str(test) + "\n")
+  logFile.write("="*30 + "\n")
+  for key in userList:
+    user = userList[key]
+    toaddr = user['email']
+    print(f"Send {key} {toaddr}")
+
+    if len(toaddr) == 0:
+        continue
+#    toaddr = "linupa1@gmail.com"
+    fromaddr = GOOGLE_ACCOUNT
+    sub = subject
+    content = getBody(user)
+
+    if verbose:
+        print(sub)
+        print(content)
+    msg = gmail.CreateMessage(fromaddr, toaddr, sub, content)
+    if not test:
+        gmail.SendMessage(GOOGLE_ACCOUNT, msg)
+    else:
+        print("Skip sending")
+    logFile.write(content + "\n")
+    time.sleep(0.01)
+    count += 1
+#    if count > 5:
+#        break;
+  logFile.close()
+
+if __name__ == '__main__':
+
+  if len(sys.argv) < 2:
+    exit(-1)
+
+  msgTypes = {"notice": 0, "checkout": 1}
+  msgType = sys.argv[1]
+  if msgType not in msgTypes:
+    exit(-1)
+
+  test = True
+  verbose = False
+  if len(sys.argv) > 2:
+      if sys.argv[2] == "send":
+          test = False
+      elif sys.argv[2] == "verbose":
+          verbose = True
+
+  if len(sys.argv) > 3:
+      if sys.argv[3] == "send":
+          test = False
+      elif sys.argv[3] == "verbose":
+          verbose = True
+
+  print("Open MongoDB")
+  password = os.environ["MONGODB_PASSWORD"]
+  connection = 'mongodb+srv://linupa:{}@hkmcclibrary.s59ur1w.mongodb.net/?retryWrites=true&w=majority'.format(password)
+  db = MongoDB(connection)
+
+
+  print(f"Msg type: {msgType}")
+  msgType = msgTypes[msgType]
+  userList= dict()
+
+  textFile =  open('text.json', 'r', encoding='utf-8')
+  text = json.load(textFile)['kr']
+
+  if msgType == 0:
+      subject = text["courtesyNotice"]
+      f2 = open('CourtesyNotice.txt', 'r', encoding='utf-8')
+      body = f2.read()
+  else:
+      subject = text["rentalHistory"]
+      f3 = open('CheckInOut.txt', 'r', encoding='utf-8')
+      body = f3.read()
+
+#  print(sys.path[1])
+#  directory = sys.path[1]
+#  os.chdir(sys.path[1])
+
+  directory = os.getcwd()
+  directory = sys.version
+
+  previewIndex = 0
+
+  def setReceiver(db, arg):
+    today = datetime.now().date()
+    if arg == 0:
+        return db.getRecentDueDate(today, 7)
+#        reqtype = chaekbangdb_pb2.COURTECY_NOTICE
+    else:
+        return db.getRecentActivity(today, 7)
+#        reqtype = chaekbangdb_pb2.RENTAL_LOG
+#    userList = db.GetUserList(reqtype)
+#    return userList
+
+  def getBody(entry):
+     writer = EmailWriter()
+     txt = body
+     context = {
+        'user': {'id': entry['id'], 'name': entry['name']},
+        'content': ""
+     }
+     if verbose:
+         print(entry)
+
+     if msgType == 0:
+       writer = EmailWriter()
+       rentList = ""
+       for log in entry['rent']:
+#         print(log)
+         book = writer.Render("item.txt", log)
+         rentList += book
+       context.update({'content': rentList})
+     elif msgType == 1:
+       rentList = ""
+       returnList = ""
+       for log in entry['rent']:
+#         print(log)
+         book = writer.Render("book.txt", log)
+         if log['log']["book_state"] == 0:
+            returnList += book
+         elif log['log']["book_state"] == 1:
+            rentList += book
+#       context.update(db.GetLogString(entry['rent']))
+       context.update({'checkout': rentList, 'checkin': returnList })
+
+     f = codecs.open('./cache.txt', 'w', 'utf-8')
+     f.write(txt);
+     f.close()
+
+     return writer.Render('cache.txt', context)
+
+  userList = setReceiver(db, msgType)
+
+  sendEmail(userList, test)
+
