e48ec803e55611cfdadbb75baf71f9cb187dc365
Kwan Suk Kim <linupa@gmail.com>
2023-12-10 13:12:23 -0800

    Added timeChecker

---
+++ timeChecker.py
@@ -0,0 +1,36 @@
+import time
+import os
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+if __name__ == "__main__":
+    tz = ZoneInfo("America/Los_Angeles")
+#    while True:
+    currentTime = datetime.now(tz=tz)
+    print(currentTime)
+    hour = currentTime.hour
+    weekday = currentTime.weekday()
+
+    if weekday == 1:
+        print("Send notice test")
+        cmd = "python3 sender.py notice"
+        print(os.popen(cmd).read())
+    elif weekday == 2:
+        print("Send checkout test")
+        cmd = "python3 sender.py checkout"
+        print(os.popen(cmd).read())
+    elif weekday == 4:
+        print("Send notice test")
+        cmd = "python3 sender.py notice"
+        print(os.popen(cmd).read())
+    elif weekday == 5:
+        print("Send notice (Satureday)")
+        cmd = "python3 sender.py notice" # send"
+        print(os.popen(cmd).read())
+    elif weekday == 6:
+        print("Send checkout (Sunday)")
+        cmd = "python3 sender.py checkout" # send"
+        print(os.popen(cmd).read())
+    else:
+        print(f"Do nothing ({weekday})")
+
