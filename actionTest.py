3595ca9d59fdb845805bcb0418fb7587eca1447a
Kwan Suk Kim <linupa@gmail.com>
2023-12-10 00:51:13 -0800

    Added gmail access

--- actionTest.py
+++ actionTest.py
@@ -1,6 +1,8 @@
 import os
+import json
 from datetime import datetime, timedelta
 from mongodb import MongoDB
+from gmail3 import GMail
 
 if __name__ == '__main__':
     print(f"Date {datetime.now()}")
@@ -10,5 +12,12 @@ if __name__ == '__main__':
         db = MongoDB(connection)
     except KeyError:
         print("Cannot find MongoDB credential")
+
+    try:
+        tokenStr = os.environ["MAIL_TOKEN"]
+        token = json.loads(tokenStr)
+        gmail = GMail(token)
+    except KeyError:
+        print("Cannot find mail token")
 
 
