bc4792dfa350581a0ad42acb0648174e080211ef
Kwan Suk Kim <linupa@gmail.com>
2024-04-07 16:30:06 -0700

    Update

--- timeChecker.py
+++ timeChecker.py
@@ -43,5 +43,5 @@ if __name__ == "__main__":
             cmd += " send"
         print(os.popen(cmd).read())
     else:
-        print(f"Do nothing ({weekday})")
+        print(f"Do nothing {weekdays[weekday]}({weekday})")
 
